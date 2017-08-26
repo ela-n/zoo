a1e7d4ee4669bb9502f251677b6ca0b57e719013
ela-n <ela.nogiel@gmail.com>
2017-08-26 13:29:14 +0200

    added methods

---
+++ src/com/zoo/Animals/rare/SyberianTiger.java
@@ -0,0 +1,16 @@
+package com.zoo.Animals.rare;
+
+import com.zoo.Animals.Tiger;
+
+public class SyberianTiger extends Tiger {
+    public SyberianTiger(String name, Integer hungerLevel) {
+        super(name, hungerLevel);
+
+    }
+
+    @Override
+    public void makeSound() {
+        super.makeSound();
+        System.out.println("Siberian Tiger makes sound");
+    }
+}
